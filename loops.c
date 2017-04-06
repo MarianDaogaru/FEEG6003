492d58ce78b96fb5d7492e20fc9735aca864e9f2
marian daogaru <mdaogaru@gmail.com>
2017-04-06 13:27:42 +0300

    update to static & added auto
    finished properly making static & added auto.
    now it actually threads the for loop, while single for the rest.

--- loops.c
+++ loops.c
@@ -16,6 +16,8 @@ void loop1(void);
 void loop2(void);
 void loop1_static(void);
 void loop2_static(void);
+void loop1_auto(void);
+void loop2_auto(void);
 void valid1(void);
 void valid2(void);
 
@@ -28,34 +30,69 @@ int main(int argc, char *argv[]) {
 
   init1();
 
-  #pragma opm parallel default(none) private(start1, start2, end1, end2) shared(a, b)
+  #pragma omp parallel default(none) shared(a, b, c, start1, start2, end1, end2, l1_t_def, l2_t_def) // private(start1, start2, end1, end2, l1_t_def, l2_t_def)
   {
-  start1 = omp_get_wtime();
+    printf("threads in parallel = %d \n", omp_get_thread_num());
+    #pragma omp single
+    {
+      printf("thread in 1st single = %d", omp_get_thread_num());
+      start1 = omp_get_wtime();
 
-  for (r=0; r<reps; r++){
-    loop1();
-  }
 
-  end1  = omp_get_wtime();
+      for (int r=0; r<reps; r++){
+        loop1();
+      }
 
-  valid1();
+      end1  = omp_get_wtime();
 
-  printf("Total time for %d reps of loop 1 = %f\n",reps, (float)(end1-start1));
-  l1_t_def = end1 - start1;
-// STATIC
-init1();
-  start1 = omp_get_wtime();
+      valid1();
 
-  for (r=0; r<reps; r++){
-    loop1_static();
-  }
+      printf("Total time for %d reps of loop 1 = %f\n",reps, (float)(end1-start1));
+      l1_t_def = end1 - start1;
 
-  end1  = omp_get_wtime();
+    // STATIC
+    init1();
+      start1 = omp_get_wtime();
+    printf("before close single thread = %d \n", omp_get_thread_num());
+    } //close first single
+    printf("after close single thread = %d \n", omp_get_thread_num());
 
-  valid1();
+    #pragma omp barrier
+    {
+    for (int r=0; r<reps; r++){
+      loop1_static();
+    }
+    }
+printf("before open 2nd single thread = %d \n", omp_get_thread_num());
+  #pragma omp single
+  {
+    printf("after open 2nd single thread = %d \n", omp_get_thread_num());
+    end1  = omp_get_wtime();
+    printf("%f %f \n", start1, end1);
+    valid1();
 
-  printf("Total time for %d reps of loop 1 with STATIC = %f\n",reps, (float)(end1-start1));
+  printf("Total time for %d reps of loop 1 with STATIC  in thread %d = %f\n",reps, omp_get_thread_num(), (float)(end1-start1));
   printf("Time dif for loop1 for STATIC = %f\n", (float)(l1_t_def - (end1 - start1)));
+
+  // AUTO
+  init1();
+    start1 = omp_get_wtime();
+  }
+    #pragma omp barrier
+    {
+    for (int r=0; r<reps; r++){
+      loop1_auto();
+    }
+    }
+    #pragma omp single
+    {
+    end1  = omp_get_wtime();
+
+    valid1();
+
+    printf("Total time for %d reps of loop 1 with AUTO = %f\n",reps, (float)(end1-start1));
+    printf("Time dif for loop1 for AUTO = %f\n", (float)(l1_t_def - (end1 - start1)));
+  
 
 
 // section for loop2
@@ -64,7 +101,7 @@ init2();
 
 start2 = omp_get_wtime();
 
-for (r=0; r<reps; r++){
+for (int r=0; r<reps; r++){
   loop2();
 }
 
@@ -74,21 +111,48 @@ valid2();
 
 printf("Total time for %d reps of loop 2 = %f\n",reps, (float)(end2-start2));
 l2_t_def = end2 - start2;
+
+
 //STATIC
 init2();
 
 start2 = omp_get_wtime();
-
-for (r=0; r<reps; r++){
+}
+#pragma omp barrier
+{
+for (int r=0; r<reps; r++){
   loop2_static();
 }
-
+}
+#pragma omp single
+{
 end2  = omp_get_wtime();
 
 valid2();
 
 printf("Total time for %d reps of loop 2 STATIC = %f\n",reps, (float)(end2-start2));
 printf("Time dif for loop2 for STATIC = %f\n", (float)(l2_t_def - (end2 - start2)));
+
+//AUTO
+init2();
+
+start2 = omp_get_wtime();
+}
+#pragma omp barrier
+{
+for (int r=0; r<reps; r++){
+  loop2_auto();
+}
+}
+#pragma omp single
+{
+end2  = omp_get_wtime();
+
+valid2();
+
+printf("Total time for %d reps of loop 2 AUTO = %f\n",reps, (float)(end2-start2));
+printf("Time dif for loop2 for AUTO = %f\n", (float)(l2_t_def - (end2 - start2)));
+}
 }
 }
 
@@ -159,9 +223,10 @@ void loop1_static(void) {
   /* the static parallelisation of the first loop.
   it uses STATIC as the parallel for kind. */
   int i,j;
-
-#pragma omp for schedule(static) private(i,j)
+//printf("thread = %d \n", omp_get_thread_num());
+#pragma omp for schedule(static) //private(i,j)
   for (i=0; i<N; i++){
+    //printf("thread = %d i = %d \n", omp_get_thread_num(), i);
     for (j=N-1; j>i; j--){
       a[i][j] += cos(b[i][j]);
     }
@@ -171,11 +236,12 @@ void loop1_static(void) {
 
 
 void loop2_static(void) {
+  /* loop2 using STATIC */
   int i,j,k;
   double rN2;
 
   rN2 = 1.0 / (double) (N*N);
-#pragma omp fir schedule(static) private(i, j, k)
+#pragma omp fir schedule(static) //private(i, j, k)
   for (i=0; i<N; i++){
     for (j=0; j < jmax[i]; j++){
       for (k=0; k<j; k++){
@@ -186,6 +252,40 @@ void loop2_static(void) {
 
 }
 
+
+//AUTO
+void loop1_auto(void) {
+  int i,j;
+
+#pragma omp for schedule(auto) private(i,j)
+  for (i=0; i<N; i++){
+    for (j=N-1; j>i; j--){
+      a[i][j] += cos(b[i][j]);
+    }
+  }
+
+}
+
+void loop2_auto(void) {
+  int i,j,k;
+  double rN2;
+
+  rN2 = 1.0 / (double) (N*N);
+
+#pragma omp for schedule(static) private(i,j,k)
+  for (i=0; i<N; i++){
+    for (j=0; j < jmax[i]; j++){
+      for (k=0; k<j; k++){
+	c[i] += (k+1) * log (b[i][j]) * rN2;
+      }
+    }
+  }
+
+}
+
+
+
+//valid part
 void valid1(void) {
   int i,j;
   double suma;
